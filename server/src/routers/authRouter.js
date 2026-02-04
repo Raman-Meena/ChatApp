dd2c89599f88d764f7c7f30d6bcf97479f87d070
Raman-Meena <meenaraman687@gmail.com>
2026-02-04 14:51:56 +0530

    feat: initialize server with user authentication
    - Added package.json for server configuration and dependencies.
    - Implemented Cloudinary configuration for image uploads.
    - Created MongoDB connection setup.
    - Developed user registration, login, and logout functionalities in authController.
    - Defined user model schema for MongoDB.
    - Set up authentication routes for user registration, login, and logout.

---
+++ server/src/routers/authRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  UserRegister,
+  UserLogin,
+  UserLogout,
+} from "../controller/authController.js";
+
+const router = express.Router();
+
+router.post("/register", UserRegister);
+router.post("/login", UserLogin);
+router.get("/logout", UserLogout);
+
+
+export default router;
